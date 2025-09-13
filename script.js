c46aad6e08e1a204bf15979532e6b1d50618133e
ulnazseitzhan <ulnazzzseitzh@gmail.com>
2025-09-13 16:49:48 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector("button");
+    button.addEventListener("click", function () {
+        alert("Thank you for your feedback!");
+    });
+});
